8a2323bf47971114a0e75b8e4db67cf0c36f595d
matheuslacerdaaa <ra49001265863@acaonsfatima.org.br>
2024-11-21 11:44:11 -0300

    kkkkkkk

--- backend/src/script.sql
+++ backend/src/script.sql
@@ -2,73 +2,59 @@ create database bestburgers;
 use bestburgers;
 
 
-create table tb_vendas(
-id_vendas int primary key auto_increment,
-ds_data varchar(200),
-ds_produto varchar(200),
-nr_quantidade int,
-ds_valor decimal(16,2),
-nm_pagamento varchar (200),
-nm_vendedor varchar (200)
+drop table tb_inventario;
+drop table tb_vendas;
+drop table tb_despesas;
 
-
+create table tb_inventario(
+id_inventario int primary key auto_increment,
+ds_entrada varchar (200),
+ds_descricao varchar(200),
+nm_nota varchar(200),
+ds_validade varchar(200),
+ds_quantidade int,
+ds_valor decimal(16,2)
 );
+
+
+
 
 
 create table tb_despesas (
 id_despesas int primary key auto_increment,
 ds_data varchar(200),
 nm_descricao  varchar (200),
-ds_categoria		varchar(200),	
-ds_valor	int,
-nm_responsavel	varchar(200),
-ds_pagamento	varchar(200)
+ds_categoria    varchar(200),
+ds_valor    decimal(16,2),
+nm_responsavel    varchar(200),
+ds_pagamento    varchar(200)
 );
 
 
 
-create table tb_inventario(
-id_inventario int primary key auto_increment,
-ds_entrada varchar (200),
-ds_descricao varchar(200),
-nm_nota varchar(200),	
-ds_validade varchar(200),
-ds_quantidade int,
-ds_valor int
+
+create table tb_vendas(
+id_vendas int primary key auto_increment,
+ds_data varchar(200),
+ds_produto varchar (200),
+nr_quantidade int ,
+ds_valor decimal(16,2),
+nm_pagamento varchar(200),
+nm_vendedor varchar (200)
 );
 
 
 
 create table tb_cadastro(
-id_cadastro int primary key auto_increment,
-nm_nome varchar(200),
-ds_email varchar(200),	
-ds_senha varchar(200)
+    id_cadastro int primary key auto_increment,
+    nm_nome varchar(200),
+    ds_email varchar(200),
+    ds_senha varchar(200)
 );
 
 
 create table tb_usuario(
-id_usuario int primary key auto_increment,
-nm_usuario varchar(200),
-ds_senha varchar(200)
+    id_usuario int primary key auto_increment,
+    nm_usuario varchar(200),
+    ds_senha varchar(200)
 );
-
-
-
-
-
-
-
-
-   
-
-
-
-
-
-
-
-
-
-
-                
